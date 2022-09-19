8b1308bedcb516a12904baf269c1f7e99adf4c45
JulioDesar <cesarj598@gmail.com>
2022-09-19 00:44:49 -0300

    final version

--- Program.cs
+++ Program.cs
@@ -17,7 +17,9 @@ namespace Ivory.TesteEstagio.CampoMinado
             int coluna = rnd.Next(0, 9);
             string[] campo = campoMinado.Tabuleiro.Split("\r\n");
             AtribuirCampo(campo);
+            DestacarBandeira();
 
+            //realizara todo loop do jogo enquanto a bomba não for encontrada ou todas as casas seguras forem abertas
             while (campoMinado.JogoStatus == 0)
             {
                 if (CampoTabuleiro[linha, coluna] == "-")
@@ -32,6 +34,7 @@ namespace Ivory.TesteEstagio.CampoMinado
                     linha = rnd.Next(0, 9);
                     coluna = rnd.Next(0, 9);
                     Console.WriteLine("");
+                    DestacarBandeira();
                     Thread.Sleep(2000);
 
                 }
@@ -43,28 +46,40 @@ namespace Ivory.TesteEstagio.CampoMinado
 
             }
 
+            if(campoMinado.JogoStatus == 1)
+            {
+                Console.WriteLine("----------- Status: Vitoria -----------");
+            } else
+            {
+                Console.WriteLine("----------- Status: Game Over -----------");
+            }
+
             Console.WriteLine(campoMinado.Tabuleiro);
 
         }
 
+        /// <summary>
+        /// Método responsável por validar a entrada da jogada se for valida
+        /// </summary>
+        /// <param name="campoMinado"></param>
+        /// <param name="linha"></param>
+        /// <param name="coluna"></param>
         public static void Jogar(CampoMinado campoMinado, int linha, int coluna)
         {
             if (campoMinado.JogoStatus == 0)
             {
                 if (Bandeiras[linha, coluna] == false)
                 {
-
-
-                    
                     campoMinado.Abrir(linha + 1, coluna + 1);
-
-
                 }
-
             }
         }
 
-        public static void AtribuirCampo(String[] campo)
+        /// <summary>
+        /// Método responsável por Transformar o vetor de string em uma matriz 
+        /// </summary>
+        /// <param name="campo"></param>
+        public static void AtribuirCampo(string[] campo)
         {
             for (var i = 0; i < 9; i++)
             {
@@ -118,6 +133,9 @@ namespace Ivory.TesteEstagio.CampoMinado
             }
         }
 
+        /// <summary>
+        /// Retorna o tabuleiro atual em modo texto.
+        /// </summary>
         public static string Tabuleiro
         {
             get
@@ -141,6 +159,11 @@ namespace Ivory.TesteEstagio.CampoMinado
             }
         }
 
+        /// <summary>
+        /// Retorna o tabuleiro de bandeiras atual em modo texto:
+        /// - False caso não haja uma mina
+        /// - True caso tenha uma mina
+        /// </summary>
         public static string ImprimirBandeiras
         {
             get
@@ -164,6 +187,13 @@ namespace Ivory.TesteEstagio.CampoMinado
             }
         }
 
+
+        /// <summary>
+        /// Método responsável por Atribuir uma bandeira a uma possível mina
+        /// a ideia é pegar um numero do meio em uma matriz 3x3 e usa a estrategia Singles
+        /// </summary>
+        /// <param name="linha"></param>
+        /// <param name="coluna"></param>
         public static void AtribuirBandeira(int linha, int coluna)
         {
             if (linha == 0 && coluna == 0)
@@ -191,15 +221,19 @@ namespace Ivory.TesteEstagio.CampoMinado
                 {
 
                     Bandeiras[linha, coluna] = true;
+                    Bandeiras[linha - 1, coluna] = true;
+                    Bandeiras[linha, coluna - 1] = true;
+                    Bandeiras[linha - 1, coluna - 1] = true;
 
                 }
 
                 if (CampoTabuleiro[linha, coluna] != "0")
                 {
 
-                    Bandeiras[linha, coluna + 1] = false;
-                    Bandeiras[linha + 1, coluna] = false;
-                    Bandeiras[linha + 1, coluna + 1] = false;
+                    Bandeiras[linha, coluna] = false;
+                    Bandeiras[linha - 1, coluna] = false;
+                    Bandeiras[linha, coluna - 1] = false;
+                    Bandeiras[linha - 1, coluna - 1] = false;
                 }
             }
 
@@ -230,6 +264,26 @@ namespace Ivory.TesteEstagio.CampoMinado
                     Bandeiras[linha + 1, coluna + 1] = false;
                 }
             }
+        }
+
+        /// <summary>
+        /// Método responsável por Atribuir uma bandeira antes da jogada começar
+        /// </summary>
+        public static void DestacarBandeira()
+        {
+
+            for (var x = 0; x < 9; x++)
+            {
+                for (var y = 0; y < 9; y++)
+                {
+                    if (CampoTabuleiro[x, y] == "-")
+                    {
+                        Bandeiras[x, y] = true;
+                    }
+
+                }
+            }
+
         }
 
         private static readonly bool[,] Bandeiras = new bool[9, 9] {
